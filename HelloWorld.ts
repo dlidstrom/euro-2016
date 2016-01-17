0da75f78b58d384cf79f68811567cbf078af3e17
Daniel Lidström <daniel@Daniels-MBP.lan>
2016-01-17 13:33:18 +0100

    Got tasks working

---
+++ HelloWorld.ts
@@ -0,0 +1,11 @@
+class Startup {
+    public static main(): number {
+        console.log('Hello, World!');
+        
+        console.log('Running 5...');
+        
+        console.log('Running 5...');
+        
+        return 5;
+    }
+}
---
+++ Main.ts
@@ -0,0 +1,8 @@
+class MyClass {
+    name: string;
+    age: number;
+    
+    constructor() {
+        console.log('Hello wxyz!')
+    }
+}
